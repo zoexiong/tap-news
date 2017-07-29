28bd36d3cfa7be8997279d5b43b32ca9ac26433a
zoexiong <xiongsy@uw.edu>
2017-07-28 17:23:29 -0700

    add materialize library and logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
 import React from 'react';
-import logo from '../public/logo.png';
+import logo from './img/logo.png';
 import './App.css';
 
 //import NewsPanel from '../NewsPanel/NewsPanel';
@@ -13,7 +13,6 @@ class App extends React.Component{
         <div>
           <img className='logo' src={logo} alt='logo'/>
           <div className='container'>
-            {/*<NewsPanel />*/}
           </div>
         </div>
     );
