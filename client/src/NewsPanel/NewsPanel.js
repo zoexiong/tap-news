41c311b2923cb7a02d149dd628f61566791c73f7
zoexiong <xiongsy@uw.edu>
2017-07-28 23:58:28 -0700

    add debounce

--- client/src/NewsPanel/NewsPanel.js
+++ client/src/NewsPanel/NewsPanel.js
@@ -1,7 +1,6 @@
 import './NewsPanel.css';
 import React from 'react';
-
-//import _ from 'lodash';
+import _ from 'lodash';
 
 import NewsCard from '../NewsCard/NewsCard'
 
@@ -9,22 +8,27 @@ class NewsPanel extends React.Component{
     constructor() {
         super();
         this.state = {news:null};
-        //this.handleScroll = this.handleScroll.bind(this);
+        //we used this.loadMoreNews() in this.handleScroll, so we need to bind it in constructor
+        this.handleScroll = this.handleScroll.bind(this);
     }
 
     componentDidMount() {
         this.loadMoreNews();
-        //this.loadMoreNews = _.debounce(this.loadMoreNews, 1000);
-        //window.addEventListener('scroll', this.handleScroll);
+        //use debounce to limit number of request send in 1 sec to 1
+        this.loadMoreNews = _.debounce(this.loadMoreNews, 1000);
+        window.addEventListener('scroll', this.handleScroll);
     }
 
-    // handleScroll() {
-    //     let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-    //     if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
-    //         console.log('Loading more news');
-    //         this.loadMoreNews();
-    //     }
-    // }
+    //handle browser scroll
+    handleScroll() {
+        //get the first value that is not null (for compatibility)
+        let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+        //height of window + length already scrolled >= whole length of the page, use 50 as buffer
+        if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
+            console.log('Loading more news');
+            this.loadMoreNews();
+        }
+    }
 
     loadMoreNews() {
         let request = new Request('http://localhost:3000/news', {
@@ -45,7 +49,9 @@ class NewsPanel extends React.Component{
     renderNews() {
         let news_list = this.state.news.map(function(news) {
             return(
-                <a className='list-group-item' key={news.digest} href="#">
+                //this key should be existing and unique to tell react whether this item is already existing in the list
+                /*<a className='list-group-item' key={news.digest} href="#">*/
+                    <a className='list-group-item' href="#">
                     <NewsCard news={news} />
                 </a>
             );
